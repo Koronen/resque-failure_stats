980345fadf8016c0165e9b90c0510d8820599da3
Victor Koronen <koronen@kth.se>
2015-02-03 17:50:27 +0100

    Properly get all failures

--- lib/resque/plugins/failure_stats/server.rb
+++ lib/resque/plugins/failure_stats/server.rb
@@ -10,7 +10,7 @@ module Resque
         class << self
           def registered(app)
             app.get '/failure_stats' do
-              failures = Resque::Failure.all(0, 9999).each_with_index.map { |ex, i| ex['id'] = i; ex }
+              failures = Resque::Failure.all(0, -1).each_with_index.map { |ex, i| ex['id'] = i; ex }
               @fbe = Hash.new { |h, k| h[k] = Array.new }
               @fbc = Hash.new { |h, k| h[k] = Array.new }
               failures.each { |ex| @fbe[ex['exception']] << ex; @fbc[ex['payload']['class']] << ex }
